6e4875f896bdf80d118e0ac1b3eba505725701ae
Jeff Mendez <jeffmendez19@gmail.com>
2021-05-31 21:32:56 -0400

    chore(scan): fix scan endpoint for extension

--- public/popup.js
+++ public/popup.js
@@ -1,49 +1,35 @@
-let changeColor = document.getElementById("changeColor");
-let scanPage = document.getElementById("scanPage");
+// let changeColor = document.getElementById('changeColor')
+let scanPage = document.getElementById('scanPage')
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
+chrome.storage.sync.get('color', ({ color }) => {
+    scanPage.style.backgroundColor = color
+})
 
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
-    });
-  });
-  
+scanPage.addEventListener('click', async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
 
-  scanPage.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: getPageIssues,
-    });
-  });
-  
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: getPageIssues,
+  })
+})
 
 async function getPageIssues() {
-    try {
-        const response = await fetch(`https://api.a11ywatch.com/api/scanWebsiteAsync?url=${encodeURI(location.href)}`)
-        const data = await response.json();
-        let scanResults = document.getElementById("scanResults");
+  try {
+    const response = await fetch(
+      `https://api.a11ywatch.com/api/scanWebsiteAsync?websiteUrl=${encodeURI(
+        location.href
+      )}`
+    )
+    const data = await response.json()
+    let scanResults = document.getElementById('scanResults')
 
-        console.log(data)
+    console.log(data)
 
-        if (scanResults) {
-            scanResults.innerHTML = JSON.stringify(data);
-        }
-        
-    } catch (e) {
-        console.error(e)
+    if (scanResults) {
+      scanResults.innerHTML = JSON.stringify(data)
     }
-}
-
-function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-        document.body.style.backgroundColor = color;
-    });
+  } catch (e) {
+    console.error(e)
+  }
 }
