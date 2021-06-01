5e629b13a2c71a6c31fb08e52cac508d6a5eb2d1
Jeff Mendez <jeffmendez19@gmail.com>
2021-06-01 00:07:06 -0400

    chore(ext): add fetchable extension

--- public/popup.js
+++ public/popup.js
@@ -1,35 +1,48 @@
 // let changeColor = document.getElementById('changeColor')
 let scanPage = document.getElementById('scanPage')
+let scanResults = document.getElementById('a11yScanResults')
+let a11yPageScanned = document.getElementById('a11yPageScanned')
+
+chrome.runtime.onMessage.addListener(function(details) {
+    // alert('Message from frame: ' + details.data);
+    const dataParsed = JSON.parse(details.data);
+    a11yPageScanned.innerHTML = dataParsed.url
+    scanResults.innerHTML = JSON.stringify(dataParsed)
+});
 
 chrome.storage.sync.get('color', ({ color }) => {
-    scanPage.style.backgroundColor = color
+  scanPage.style.backgroundColor = color
 })
 
 scanPage.addEventListener('click', async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-
+   
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     function: getPageIssues,
   })
 })
 
-async function getPageIssues() {
-  try {
-    const response = await fetch(
-      `https://api.a11ywatch.com/api/scanWebsiteAsync?websiteUrl=${encodeURI(
-        location.href
-      )}`
-    )
-    const data = await response.json()
-    let scanResults = document.getElementById('scanResults')
-
-    console.log(data)
-
-    if (scanResults) {
-      scanResults.innerHTML = JSON.stringify(data)
+const getPageIssues = async () => {
+    try {
+      const response = await fetch(
+        `https://api.a11ywatch.com/api/scanWebsiteAsync?websiteUrl=${encodeURI(
+          location.href
+        )}`,
+        {
+          method: 'POST',
+        }
+      )
+      const data = await response.json()
+  
+      console.log(data)
+  
+      if (data.website) {
+        const web =  data.website
+        const dataSource =  JSON.stringify(Object.assign({}, web, {html: null}));
+        chrome.runtime.sendMessage({sendBack:true, data:dataSource});
+      }
+    } catch (e) {
+      console.error(e)
     }
-  } catch (e) {
-    console.error(e)
   }
-}
