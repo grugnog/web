8a223608486a436d54c467c830d1046deb0f2df4
Jeff Mendez <jeffmendez19@gmail.com>
2021-06-06 11:58:39 -0400

    chore(vimeo): add prog play for video

--- src/components/cta/cta-video.tsx
+++ src/components/cta/cta-video.tsx
@@ -4,9 +4,10 @@
  * LICENSE file in the root directory of this source tree.
  **/
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import VisibilitySensor from 'react-visibility-sensor'
+import Player from '@vimeo/player'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -34,6 +35,10 @@ const useStyles = makeStyles((theme) => ({
   video: {
     width: '100%',
     height: '100%',
+    '& iframe': {
+      width: '100%',
+      height: '100%',
+    },
   },
   frame: {
     border: 0,
@@ -41,32 +46,58 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-export function CtaVideo() {
+function Inner({ isVisible }: { isVisible: boolean }) {
   const classes = useStyles()
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    const video = document.querySelector('iframe') as HTMLIFrameElement
+
+    if (!loaded && isVisible) {
+      setLoaded(true)
+    }
+
+    const player = loaded ? new Player(video) : null
+
+    if (video && player) {
+      if (isVisible && player?.play) {
+        player.play()
+      } else if (!isVisible && player?.pause) {
+        player.pause()
+      }
+    }
+  }, [isVisible])
+
   const videoClassName = `${classes.video} ${classes.frame}`
 
   return (
-    <VisibilitySensor partialVisibility>
-      {({ isVisible }) => (
-        <div className={classes.root} id='video-section'>
-          <div className={classes.card}>
-            <div className={`${classes.float} ${classes.video}`}>
-              <div className={classes.video}>
-                {isVisible ? (
-                  <iframe
-                    src={`https://player.vimeo.com/video/389034032?title=0&byline=0&portrait=0&muted=1&autoplay=${1}&controls=0&loop=1&texttrack=en`}
-                    allowFullScreen
-                    title='A11yWatch demo video'
-                    className={videoClassName}
-                  />
-                ) : (
-                  <div className={videoClassName} />
-                )}
-              </div>
-            </div>
+    <div className={classes.root} id='video-section'>
+      <div className={classes.card}>
+        <div className={`${classes.float} ${classes.video}`}>
+          <div className={classes.video}>
+            {loaded ? (
+              <iframe
+                src={
+                  'https://player.vimeo.com/video/389034032?title=0&byline=0&portrait=0&muted=1&autoplay=1&controls=0&loop=1&texttrack=en'
+                }
+                allowFullScreen
+                title='A11yWatch demo video'
+                className={videoClassName}
+              />
+            ) : (
+              <div className={videoClassName} />
+            )}
           </div>
         </div>
-      )}
+      </div>
+    </div>
+  )
+}
+
+export function CtaVideo() {
+  return (
+    <VisibilitySensor partialVisibility>
+      {({ isVisible }) => <Inner isVisible={isVisible} />}
     </VisibilitySensor>
   )
 }
