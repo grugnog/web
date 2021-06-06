9a5eecbe86090ae8fffbbd586ea0e07129230eca
Jeff Mendez <jeffmendez19@gmail.com>
2021-06-06 17:04:41 -0400

    remove unused memo on marketing testi

--- src/components/cta/cta-search.tsx
+++ src/components/cta/cta-search.tsx
@@ -4,7 +4,7 @@
  * LICENSE file in the root directory of this source tree.
  **/
 
-import React, { useMemo } from 'react'
+import React from 'react'
 import { Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import { strings } from '@app-strings'
