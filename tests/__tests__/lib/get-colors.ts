c482898b78d023ed5b355ba46b95f1e880f75920
Jeff Mendez <jeffmendez19@gmail.com>
2021-07-05 12:05:38 -0400

    add just lib test get-colors

---
+++ tests/__tests__/lib/get-colors.ts
@@ -0,0 +1,21 @@
+import { getAccessibleColors } from '@app/lib'
+
+describe('get-colors util', () => {
+  it('renders without crashing', () => {
+    const colors = getAccessibleColors({
+      parentColor: '#000',
+      elementColor: '#ccc',
+    })
+    console.log(colors)
+
+    const value = {
+      background: { model: 'rgb', color: [0, 0, 0], valpha: 1 },
+      contrastRatio: Infinity,
+      foreground: { model: 'rgb', color: [204, 204, 204], valpha: 1 },
+      l1: 0,
+      l2: 0.6038273388553377,
+    }
+
+    expect(colors).toEqual(value)
+  })
+})
--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -1,2 +1,55 @@
 import '@testing-library/jest-dom/extend-expect'
-import './__utils__/describe'
+import preloadAll from 'jest-next-dynamic'
+import { createElement, FC } from 'react'
+import { render, screen } from '@testing-library/react'
+
+beforeAll(async () => {
+  await preloadAll()
+})
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      prefetch: () => null,
+    }
+  },
+}))
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      describePage?: any
+    }
+  }
+}
+
+interface Target {
+  component: FC
+  folder?: string
+  name?: string
+}
+
+global.describePage = jest.fn(
+  ({ component, folder, name: target }: Target, callBack?: () => void) => {
+    const name = target || (component && component.displayName)
+
+    describe((folder || name).toUpperCase(), () => {
+      it('renders without crashing', () => {
+        render(
+          createElement(component || require(`@app/pages/${folder}`).default, {
+            name,
+          })
+        )
+        expect(screen.getByRole('heading', { level: 1 })).toBeInTheDocument()
+
+        if (typeof name !== 'undefined') {
+          expect(screen.getByRole('heading', { name })).toBeInTheDocument()
+        }
+
+        if (typeof callBack === 'function') {
+          jest.fn(callBack)
+        }
+      })
+    })
+  }
+)
