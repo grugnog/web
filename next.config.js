87f8829405e93da23b8e8caaadf94a8c1a575d55
j-mendez <jeff@a11ywatch.com>
2022-01-25 21:49:21 -0500

    chore(security): add security headers for transport

--- next.config.js
+++ next.config.js
@@ -72,6 +72,10 @@ const securityHeaders = [
     key: 'X-DNS-Prefetch-Control',
     value: 'on',
   },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
 ]
 
 module.exports = withPWA({
