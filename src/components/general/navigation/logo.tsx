ad49c21ff72c279e34a69779205084e639b96d6e
j-mendez <jeff@a11ywatch.com>
2022-03-14 14:02:34 -0400

    chore(logo): update logo

--- src/components/general/navigation/logo.tsx
+++ src/components/general/navigation/logo.tsx
@@ -2,19 +2,14 @@ import React from 'react'
 
 export const Logo = () => {
   return (
-    <img
-      src='/img/logo_dark.svg'
-      alt='A11yWatch logo'
-      width={'51'}
-      height={'30'}
-    />
+    <img src='/img/logo.svg' alt='A11yWatch logo' width={'51'} height={'30'} />
   )
 }
 
 export const SmallLogo = ({ className }: any) => {
   return (
     <img
-      src='/img/logo_dark.svg'
+      src='/img/logo.svg'
       alt='A11yWatch logo'
       width={'25.5'}
       height={'15'}
