6e3fc409b8727fc91ffcabb8cd1cba665364f74b
j-mendez <jeff@a11ywatch.com>
2022-04-04 16:37:54 -0400

    chore(auth): fix title align

--- src/components/marketing/marketing-short-title.tsx
+++ src/components/marketing/marketing-short-title.tsx
@@ -3,7 +3,7 @@ import { strings } from '@app-strings'
 
 export const MarketingShortTitle = () => {
   return (
-    <div className={'hidden md:block py-10 place-items-center flex flex-col'}>
+    <div className={'hidden md:block py-10 text-center'}>
       <div className={'font-bold text-2xl'}>{strings.appName}</div>
       <div className={'text-lg'}>{strings.subTitle}</div>
     </div>
