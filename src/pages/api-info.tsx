2e678b00414ff1926f024a4a38e21ee3712e5435
j-mendez <jeff@a11ywatch.com>
2022-04-28 07:58:39 -0400

    chore(docs): cleanup api docs intro

--- src/pages/api-info.tsx
+++ src/pages/api-info.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useState, useCallback } from 'react'
 import { Container, Typography, Button } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
-import { NavBar, PageTitle } from '@app/components/general'
+import { NavBar, PageTitle, Link } from '@app/components/general'
 import { Box } from '@a11ywatch/ui'
 import { TextSkeleton } from '@app/components/placeholders'
 import { UserManager } from '@app/managers'
@@ -28,9 +28,6 @@ const useStyles = makeStyles((theme) => ({
   },
   email: {
     marginBottom: theme.spacing(2),
-  },
-  bold: {
-    fontWeight: 600,
   },
   italic: {
     fontStyle: 'italic',
@@ -59,62 +56,57 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
+const SectionTitle = ({ children, className, bold }: any) => {
+  return (
+    <h2
+      className={`text-2xl ${bold ? 'font-bold' : ''}${
+        className ? ' ' + className : ''
+      }`}
+    >
+      {children}
+    </h2>
+  )
+}
+
 // TODO: GENERATE DOCS FROM API
 function Api({ name }: PageProps) {
   const classes = useStyles()
-  const { data = {}, loading } = userData()
   const [keyVisible, setKey] = useState<boolean>(false)
+
+  const { data = {}, loading } = userData()
+
   const { user } = data
 
   const toggleKey = useCallback(() => {
     setKey((c) => !c)
   }, [])
 
-  const SectionTitle = ({
-    children,
-    className,
-    variant = 'subtitle2',
-    component = 'p',
-    bold,
-  }: any) => {
-    return (
-      <Typography
-        variant={variant}
-        component={component}
-        className={`${bold ? classes.bold : ''}${
-          className ? ' ' + className : ''
-        }`}
-      >
-        {children}
-      </Typography>
-    )
-  }
-
   return (
     <Fragment>
       <NavBar
         backButton
-        title={name}
+        title={name.toUpperCase()}
         notitle
         marketingLinks={UserManager?.token ? [] : null}
       />
       <Container maxWidth='xl' className={classes.root}>
         <Box>
-          <PageTitle title={'API Information'} />
-          <SectionTitle variant='subtitle1'>
-            Add authorization header with the jwt format <i>Bearer TOKEN</i> for
-            more information check{' '}
+          <PageTitle title={'API Details and Usage'} />
+          <p className='text-lg'>
+            In order to get started using the A11yWatch API you need to add a
+            authorization header with the jwt format <b>Bearer TOKEN</b>.
+          </p>
+          <p className='text-lg'>
+            For more information check the API{' '}
             <a
               href={'https://docs.a11ywatch.com/documentation/api'}
               target='_blank'
               rel='noreferrer'
+              className='underline text-blue-600'
             >
-              Documentation
+              documentation
             </a>
-          </SectionTitle>
-          <p>
-            This document is going to be refactored soon as we restructure and
-            expose more of the main API.
+            .
           </p>
           {!data?.user && loading ? (
             <TextSkeleton className={classes.email} />
@@ -137,11 +129,18 @@ function Api({ name }: PageProps) {
               ) : null}
             </div>
           ) : null}
+        </Box>
+
+        <Box>
           <SectionTitle variant='subtitle1' bold>
             Daily Allowed Usage
           </SectionTitle>
           {!data?.user && loading ? (
             <TextSkeleton className={classes.email} />
+          ) : !data?.user ? (
+            <p className={'pb-2 text-lg'}>
+              <Link href={'/login'}>Login</Link> to see your API limits.
+            </p>
           ) : (
             <SectionTitle className={classes.email}>
               {user?.apiUsage?.usage || 0}/
@@ -155,6 +154,24 @@ function Api({ name }: PageProps) {
             </SectionTitle>
           )}
         </Box>
+
+        <Box>
+          <h3 className='text-xl font-bold'>API Endpoints</h3>
+          <h4 className='text-lg pb-1'>Scan a website for issues.</h4>
+          <code className='border block p-2 rounded bg-gray-100'>
+            {`curl --location --request POST
+            'https://api.a11ywatch.com/api/scan-simple' \ --header
+            'Content-Type: application/x-www-form-urlencoded' \ --data-urlencode
+            'url=https://www.nytimes.com'`}
+          </code>
+        </Box>
+
+        <div className='border rounded inline-block px-4 py-1'>
+          <p className='text-grey-600 text-lg'>
+            This document is going to be refactored soon as we restructure and
+            expose more of the main API.
+          </p>
+        </div>
       </Container>
     </Fragment>
   )
