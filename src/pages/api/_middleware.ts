b1c4b30e008dc7a75f401579c3ed1797dbe5db52
j-mendez <jeff@a11ywatch.com>
2022-05-02 04:54:25 -0400

    chore(marketing): update web accessibility info section

--- src/pages/api/_middleware.ts
+++ src/pages/api/_middleware.ts
@@ -4,10 +4,15 @@ import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   if (req.nextUrl.pathname === '/api/iframe') {
-    const iframeSource = await iframe(
-      req.nextUrl.searchParams.get('url') || '',
-      req.nextUrl.searchParams.get('baseHref') || ''
-    )
+    let iframeSource
+    try {
+      iframeSource = await iframe(
+        req.nextUrl.searchParams.get('url') || '',
+        req.nextUrl.searchParams.get('baseHref') || ''
+      )
+    } catch (e) {
+      console.error(e)
+    }
 
     return new NextResponse(iframeSource, {
       headers: { 'Content-Type': 'text/html' },
--- src/pages/website-accessibility-checker.tsx
+++ src/pages/website-accessibility-checker.tsx
@@ -15,22 +15,41 @@ function WebsiteAccessibilityChecker({ name }: PageProps) {
         index
       >
         <CtaIntroRest checker />
-        <div className='px-4 max-w-[800px]'>
-          <h3 className='text-3xl font-bold'>Live web accessibility testing</h3>
-          <p className='text-lg'>
-            Scan your website and get lightning fast results for free. Sign up
-            today to get monitoring and other web accessibility improvement
-            tools right in the browser or your own servers.
-          </p>
-          <h4 className='text-2xl font-bold'>
-            Real time web accessibility results
-          </h4>
-          <p className='text-lg'>
-            Optimize your workflow on tackling web inclusion issues while
-            learning how to stay accessible for the long run. If you have
-            thousands of pages, A11yWatch was built with the speed in mind to
-            give you real time results with minimal downtime.
-          </p>
+        <div className='px-4 max-w-[800px] space-y-2'>
+          <div>
+            <h3 className='text-3xl font-bold'>
+              Live web accessibility testing
+            </h3>
+            <p className='text-lg'>
+              Scan your website and get lightning fast results for free. Sign up
+              today to get site-wide monitoring and other web accessibility
+              improvement tools right in the browser or your own servers. Theres
+              multiple entry points to get started keeping your website
+              inclusive to everyone.
+            </p>
+          </div>
+          <div>
+            <h4 className='text-2xl font-bold'>
+              Real time web accessibility results
+            </h4>
+            <p className='text-lg'>
+              Optimize your workflow on tackling web inclusion issues while
+              learning how to stay accessible for the long run. If you have
+              thousands of pages, A11yWatch was built with the speed in mind to
+              give you real time results with minimal downtime.
+            </p>
+          </div>
+          <div>
+            <h4 className='text-2xl font-bold'>
+              Deliver changes with confidence
+            </h4>
+            <p className='text-lg'>
+              Extending WCAG as the foundation, we also look at other areas of
+              the guidelines that have yet to be introduced and other concepts
+              that relates into web accessibility. The recommended solutions
+              based on your web page apply the guidelines by using chromium.
+            </p>
+          </div>
         </div>
       </MarketingDrawer>
       <MarketingBottomTemporaryDrawer />
