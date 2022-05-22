103e8cfd3acec880f8ab682d56200c2d58c2ccdc
j-mendez <jeff@a11ywatch.com>
2022-05-22 18:22:57 -0400

    chore(marketing): fix web accessibility page ui default

--- src/pages/web-accessibility.tsx
+++ src/pages/web-accessibility.tsx
@@ -1,95 +1,121 @@
 import React from 'react'
-import { Typography, List, ListItem } from '@material-ui/core'
+import { Typography } from '@material-ui/core'
 import { MarketingDrawer, PageTitle } from '@app/components/general'
-import { theme } from '@app-theme'
 import { metaSetter } from '@app/utils'
 import type { PageProps } from '@app/types'
 
+const checklistLinks = [
+  { href: 'https://a11yproject.com/checklist' },
+  {
+    href:
+      'https://medium.com/@krisrivenburgh/the-ada-checklist-website-compliance-guidelines-for-2019-in-plain-english-123c1d58fad9',
+  },
+  {
+    href: 'https://developer.mozilla.org/en-US/docs/Web/Accessibility',
+  },
+  {
+    href: 'https://www.section508.gov/test/testing-overview/',
+  },
+]
+
+const linkStyle = {
+  fontSize: 16,
+  fontWeight: 600,
+}
+
+const bold = { fontWeight: 600 }
+
 function WebAccessibility({ name }: PageProps) {
-  const checklistLinks = [
-    { href: 'https://a11yproject.com/checklist' },
-    {
-      href:
-        'https://medium.com/@krisrivenburgh/the-ada-checklist-website-compliance-guidelines-for-2019-in-plain-english-123c1d58fad9',
-    },
-    {
-      href: 'https://developer.mozilla.org/en-US/docs/Web/Accessibility',
-    },
-    {
-      href: 'https://www.section508.gov/test/testing-overview/',
-    },
-  ]
-
-  const linkStyle = {
-    color: theme.palette.primary.main,
-    textDecoration: 'underline',
-    fontSize: 16,
-    fontWeight: 600,
-  }
-
-  const bold = { fontWeight: 600 }
-
   return (
     <MarketingDrawer title={name}>
       <PageTitle>Web Accessibility</PageTitle>
       <Typography variant='subtitle1' component='h2' gutterBottom>
         Improve your {`product's`} web accessibility from the start of the
         building process quickly. Follow these principles to improve your
-        websites accessibility with some building block rules.
+        websites inclusion with some foundational rules.
       </Typography>
       <Typography variant='h4' component='h3' gutterBottom style={bold}>
         Color
       </Typography>
       <Typography variant='subtitle1' component='p' gutterBottom>
-        Make clear distinct color design choices by using googles material
-        design color tool{' '}
+        Make clear distinct color design choices by using{' '}
         <a
+          className={'text-blue-600'}
           href={'https://material.io/design/color/#'}
           style={linkStyle}
           target='_blank'
           rel='noreferrer'
         >
-          https://material.io/design/color/#
+          Googles Material Design Color Tool
+        </a>{' '}
+        and{' '}
+        <a
+          className={'text-blue-600'}
+          href={'https://contrast-ratio.com/'}
+          style={linkStyle}
+          target='_blank'
+          rel='noreferrer'
+        >
+          Contrast Ratio
         </a>
         .
       </Typography>
       <Typography variant='h4' component='h3' gutterBottom style={bold}>
-        Principles
+        Web Inclusion Principles
       </Typography>
       <Typography variant='subtitle1' component='p' gutterBottom>
-        Application foundations to understand to make your product reach out to
-        more audiences{' '}
-        <a
-          target='_blank'
-          href={'https://www.w3.org/WAI/fundamentals/accessibility-principles/'}
-          rel='noreferrer'
-          style={linkStyle}
-        >
-          https://www.w3.org/WAI/fundamentals/accessibility-principles/
-        </a>
-        .
+        Application foundations that help improve your audience inclusion and
+        reach.
       </Typography>
+      <ul className='space-y-2 pb-2 list-disc'>
+        <li>
+          <a
+            className={'text-blue-600'}
+            target='_blank'
+            href={
+              'https://www.w3.org/WAI/fundamentals/accessibility-principles/'
+            }
+            rel='noreferrer'
+            style={linkStyle}
+          >
+            WAI Accessibility Principles
+          </a>
+        </li>
+        <li>
+          <a
+            className={'text-blue-600'}
+            target='_blank'
+            href={'https://www.w3.org/WAI/standards-guidelines/wcag/'}
+            rel='noreferrer'
+            style={linkStyle}
+          >
+            WCAG Standards
+          </a>
+        </li>
+      </ul>
       <Typography variant='h4' component='h3' gutterBottom style={bold}>
-        Checklist
+        Accessibility Checklist
       </Typography>
       <Typography variant='subtitle1' component='p' gutterBottom>
         A couple of quick checklists to improve your contents accessibility.
       </Typography>
-      <List>
+      <ul className='space-y-2 pb-2 list-disc'>
         {checklistLinks.map(({ href }: any) => {
           return (
-            <ListItem
-              href={href}
-              component={'a'}
-              key={href}
-              target='_blank'
-              style={linkStyle}
-            >
-              {href}
-            </ListItem>
+            <li key={href}>
+              <a
+                className={'text-blue-600'}
+                href={href}
+                target='_blank'
+                style={linkStyle}
+                rel='noreferrer'
+              >
+                {href}
+              </a>
+            </li>
           )
         })}
-      </List>
+      </ul>
       <Typography variant='h4' component='h3' gutterBottom style={bold}>
         Features
       </Typography>
@@ -97,18 +123,45 @@ function WebAccessibility({ name }: PageProps) {
         Accessibility comes in many forms, with macOS/iphones you can enhance a
         major part of the application so {`it's`} best we understand the
         technologies underneath. This allows us to use these features to our
-        advantage to better assist users or ensure your product just works.
+        advantage to better assist users.
       </Typography>
-      <List>
-        <ListItem
-          href={'https://www.apple.com/accessibility/iphone/vision/'}
-          component={'a'}
-          target='_blank'
-          style={linkStyle}
-        >
-          https://www.apple.com/accessibility/iphone/vision/
-        </ListItem>
-      </List>
+      <ul className='space-y-2 pb-2 list-disc'>
+        <li>
+          <a
+            className={'text-blue-600'}
+            href={'https://www.apple.com/accessibility/iphone/vision/'}
+            target='_blank'
+            style={linkStyle}
+            rel='noreferrer'
+          >
+            Apple Accesibility Vision
+          </a>
+        </li>
+        <li>
+          <a
+            className={'text-blue-600'}
+            href={'https://www.google.com/accessibility/'}
+            target='_blank'
+            style={linkStyle}
+            rel='noreferrer'
+          >
+            Google Accesibility
+          </a>
+        </li>
+        <li>
+          <a
+            className={'text-blue-600'}
+            href={
+              'https://business.twitter.com/en/blog/our-accessibility-team-shares-best-practices.html'
+            }
+            target='_blank'
+            style={linkStyle}
+            rel='noreferrer'
+          >
+            Accessibility on Twitter
+          </a>
+        </li>
+      </ul>
       <Typography variant='h4' component='h3' gutterBottom style={bold}>
         React components with accessibility in mind
       </Typography>
@@ -117,24 +170,41 @@ function WebAccessibility({ name }: PageProps) {
         chakra-ui. They take care of a lot of the things that we can easily miss
         when trying to deliver a website fast.
       </Typography>
-      <List>
-        <ListItem
-          href={'https://chakra-ui.com'}
-          component={'a'}
-          target='_blank'
-          style={linkStyle}
-        >
-          Chakra UI
-        </ListItem>
-        <ListItem
-          href={'https://react-icons.github.io/react-icons/icons?name=gr'}
-          component={'a'}
-          target='_blank'
-          style={linkStyle}
-        >
-          GrIcons
-        </ListItem>
-      </List>
+      <ul className='space-y-2 pb-2 list-disc'>
+        <li>
+          <a
+            className={'text-blue-600'}
+            href={'https://chakra-ui.com'}
+            target='_blank'
+            style={linkStyle}
+            rel='noreferrer'
+          >
+            Chakra UI
+          </a>
+        </li>
+        <li>
+          <a
+            className={'text-blue-600'}
+            href={'https://react-icons.github.io/react-icons/icons?name=gr'}
+            rel='noreferrer'
+            target='_blank'
+            style={linkStyle}
+          >
+            GrIcons
+          </a>
+        </li>
+        <li>
+          <a
+            className={'text-blue-600'}
+            href={'https://inclusive-components.design/'}
+            rel='noreferrer'
+            target='_blank'
+            style={linkStyle}
+          >
+            Inclusive Components Desion
+          </a>
+        </li>
+      </ul>
       <Typography variant='h4' component='h3' gutterBottom style={bold}>
         Avoid Intense Animations
       </Typography>
@@ -174,57 +244,85 @@ function WebAccessibility({ name }: PageProps) {
         with this enabled from aria-label, a11y assetive props, performance and
         more.
       </Typography>
-      <List>
-        <ListItem
-          href={
-            'https://support.apple.com/guide/iphone/turn-on-and-practice-voiceover-iph3e2e415f/ios'
-          }
-          component={'a'}
-          target='_blank'
-          style={linkStyle}
-        >
-          https://support.apple.com/guide/iphone/turn-on-and-practice-voiceover-iph3e2e415f/ios
-        </ListItem>
-      </List>
+      <ul className='space-y-2 pb-2 list-disc'>
+        <li>
+          <a
+            className={'text-blue-600'}
+            href={
+              'https://support.apple.com/guide/iphone/turn-on-and-practice-voiceover-iph3e2e415f/ios'
+            }
+            target='_blank'
+            style={linkStyle}
+            rel='noreferrer'
+          >
+            Apple Turn on Voice Over
+          </a>
+        </li>
+        <li>
+          <a
+            className={'text-blue-600'}
+            href={
+              'https://support.google.com/accessibility/android/answer/6283677?hl=en'
+            }
+            target='_blank'
+            style={linkStyle}
+            rel='noreferrer'
+          >
+            Android TalkBack
+          </a>
+        </li>
+      </ul>
       <Typography variant='h4' component='h3' gutterBottom style={bold}>
         More Resources
       </Typography>
-      <List>
-        <ListItem
-          href={'https://a11yresources.webflow.io'}
-          component={'a'}
-          target='_blank'
-          style={linkStyle}
-        >
-          https://a11yresources.webflow.io
-        </ListItem>
-        <ListItem
-          href={'https://www.a11ytips.dev'}
-          component={'a'}
-          target='_blank'
-          style={linkStyle}
-        >
-          https://www.a11ytips.dev
-        </ListItem>
-        <ListItem
-          href={'https://www.getstark.co'}
-          component={'a'}
-          target='_blank'
-          style={linkStyle}
-        >
-          Stark
-        </ListItem>
-        <ListItem
-          href={
-            'https://www.websiteplanet.com/blog/website-accessibility-made-easy-ultimate-guide/'
-          }
-          component={'a'}
-          target='_blank'
-          style={linkStyle}
-        >
-          https://www.websiteplanet.com/blog/website-accessibility-made-easy-ultimate-guide/
-        </ListItem>
-      </List>
+      <ul className='space-y-2 pb-2 list-disc'>
+        <li>
+          <a
+            className={'text-blue-600'}
+            href={'https://a11yresources.webflow.io'}
+            target='_blank'
+            rel='noreferrer'
+            style={linkStyle}
+          >
+            A11y Resources
+          </a>
+        </li>
+        <li>
+          <a
+            className={'text-blue-600'}
+            href={'https://www.a11ytips.dev'}
+            target='_blank'
+            rel='noreferrer'
+            style={linkStyle}
+          >
+            A11y Tips
+          </a>
+        </li>
+        <li>
+          <a
+            className={'text-blue-600'}
+            href={'https://www.getstark.co'}
+            target='_blank'
+            style={linkStyle}
+            rel='noreferrer'
+          >
+            Stark
+          </a>
+        </li>
+        <li>
+          <a
+            className={'text-blue-600'}
+            href={
+              'https://www.websiteplanet.com/blog/website-accessibility-made-easy-ultimate-guide/'
+            }
+            target='_blank'
+            style={linkStyle}
+            rel='noreferrer'
+          >
+            Website Planet Website Accessibility Made Easy Ultimate Guide
+          </a>
+        </li>
+      </ul>
     </MarketingDrawer>
   )
 }
