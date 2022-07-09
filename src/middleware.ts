2147142ea42d3abc7d01934e6e3a634e2d3c55f4
j-mendez <jeff@a11ywatch.com>
2022-07-09 11:59:55 -0400

    chore(middleware): fix next image analytics log

--- src/middleware.ts
+++ src/middleware.ts
@@ -29,15 +29,15 @@ export async function middleware(req: NextRequest, event: NextFetchEvent) {
 
   const currentHost = req.headers?.get('host')?.replace(ROOT_URL, '')
 
-  if (/.blog/.test(currentHost + '')) {
-    const url = req.nextUrl.clone()
-    url.pathname = `/blog${req.nextUrl.pathname}`
-    res = NextResponse.rewrite(url)
-  }
-
   if (token && req.nextUrl.pathname === '/') {
     const url = req.nextUrl.clone()
     url.pathname = `/dashboard${req.nextUrl.pathname}`
+    res = NextResponse.rewrite(url)
+  }
+
+  if (/.blog/.test(currentHost + '')) {
+    const url = req.nextUrl.clone()
+    url.pathname = `/blog${req.nextUrl.pathname}`
     res = NextResponse.rewrite(url)
   }
 
@@ -50,7 +50,7 @@ export async function middleware(req: NextRequest, event: NextFetchEvent) {
   }
 
   // prevent page log on api routes
-  if (pathname !== '/api/iframe') {
+  if (['/api/iframe', '/_next/image'].includes(pathname) === false) {
     event.waitUntil(
       (async () => {
         const ua = userAgent(req)
