d3ec02a4c9a5c3837f59d9c87d117e0bd0866b7b
j-mendez <jeff@a11ywatch.com>
2022-11-23 17:42:14 -0500

    chore(app): update next@13.0.5

--- src/lib/parse-html.ts
+++ src/lib/parse-html.ts
@@ -1,9 +1,10 @@
+import { parse } from 'node-html-parser'
+
 const BLOG_URL =
   process.env.BLOG_WEBFLOW_URL || 'https://a11ywatch-blog.webflow.io'
 
 // cleanup wordpress page and parse html
 export const parseHtml = async (body: string) => {
-  const { parse } = await import('node-html-parser')
 
   const htmlRoot = parse(body)
 
